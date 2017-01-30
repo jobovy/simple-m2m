afc6d3dc270cbbeda6a10abe82ce06213cdbdcdd
Daisuke Kawata <d.kawata@ucl.ac.uk>
2017-01-30 22:26:55 +0000

    bug in calculating vz fixed.

--- simple_cylinder1d/dens1d_fstar.py
+++ simple_cylinder1d/dens1d_fstar.py
@@ -19,12 +19,15 @@ from StringIO import StringIO
 
 # input parameters
 # eplx/plx limit pi=parallax
-eplxlim=0.2
+eplxlim=0.15
 # Vt mag limit
 vtlim=11.0
 # colour range, F0V and F9V
 bvtcolmin=0.317
 bvtcolmax=0.587
+# RAVE radial velocity error limit
+hrverrlim=10.0
+hrvlim=100.0
 
 # radius of a cylinder for density calculation (kpc)
 rxylim=0.1
@@ -176,7 +179,8 @@ print 'number of all TGASxRAVE stars=',nstar_alls
 # select only stars with parallax smaller than a limit 
 epsindx=np.where((rstar['e_Plx']/rstar['Plx']<eplxlim) & (rstar['Plx']>0.0) \
   & (rstar['VTmag']<vtlim) & (rstar['BTmag']-rstar['VTmag']>bvtcolmin) \
-  & (rstar['BTmag']-rstar['VTmag']<bvtcolmax))
+  & (rstar['BTmag']-rstar['VTmag']<bvtcolmax) & (rstar['eHRV']<hrverrlim) \
+  & (np.fabs(rstar['HRV'])<hrvlim))
 
 nstar_eps=len(epsindx[0])
 
@@ -187,11 +191,16 @@ print ' number of stars after parallax cut of eplx/plx=',eplxlim,' =',nstar_eps
 ra_eps=rstar['_RAJ2000'][epsindx]
 dec_eps=rstar['_DEJ2000'][epsindx]
 plx_eps=rstar['Plx'][epsindx]
+plxerr_eps=rstar['e_Plx'][epsindx]
 vt_eps=rstar['VTmag'][epsindx]
 bt_eps=rstar['BTmag'][epsindx]
 pmra_eps=rstar['pmRA_1'][epsindx]
 pmdec_eps=rstar['pmDE'][epsindx]
+pmraerr_eps=rstar['e_pmRA'][epsindx]
+pmdecerr_eps=rstar['e_pmDE'][epsindx]
 hrv_eps=rstar['HRV'][epsindx]
+hrverr_eps=rstar['eHRV'][epsindx]
+logg_eps=rstar['logg_N_K'][epsindx]
 
 # calculate x,y,z coordinate
 # ra,dec -> glon
@@ -213,7 +222,8 @@ pmlon_eps=Tpmlb[:,0]
 pmlat_eps=Tpmlb[:,1]
 # calculate vz
 pmvconst=4.74047
-velz_eps=pmvconst*pmlat_eps/plx_eps
+velz_eps=np.cos(glat_eps)*pmvconst*pmlat_eps/plx_eps \
+ +np.sin(glat_eps)*hrv_eps
 
 # x,y,z position relative to the sun (kpc)
 rxy_eps=np.cos(glat_eps)/plx_eps
@@ -229,6 +239,21 @@ plx_cs=plx_eps[csindx]
 vt_cs=vt_eps[csindx]
 bt_cs=bt_eps[csindx]
 velz_cs=velz_eps[csindx]
+glon_cs=glon_eps[csindx]
+glat_cs=glon_eps[csindx]
+vlon_cs=pmvconst*pmlon_eps[csindx]/plx_eps[csindx]
+vlat_cs=pmvconst*pmlat_eps[csindx]/plx_eps[csindx]
+pmraerr_cs=pmvconst \
+            *np.sqrt((pmraerr_eps[csindx]**2+(pmra_eps[csindx]**2) \
+            *(plx_eps[csindx]**(-2))*(plxerr_eps[csindx]**2))\
+            *(plx_eps[csindx]**(-2)))
+pmdecerr_cs=pmvconst \
+           *np.sqrt((pmdecerr_eps[csindx]**2+(pmdec_eps[csindx]**2) \
+            *(plx_eps[csindx]**(-2))*(plxerr_eps[csindx]**2))\
+            *(plx_eps[csindx]**(-2)))
+hrverr_cs=hrverr_eps[csindx]
+hrv_cs=hrv_eps[csindx]
+logg_cs=logg_eps[csindx]
 
 nstar_cs=len(xpos_cs)
 
@@ -238,8 +263,11 @@ print ' number of stars after cylinder selection =',nstar_cs
 f=open('cylinder_starvz.asc','w')
 i=0
 while i < nstar_cs:
-  print >>f, "%f %f %f %f %f %f %f" %(xpos_cs[i],ypos_cs[i],zpos_cs[i], \
-    plx_cs[i],bt_cs[i],vt_cs[i],velz_cs[i])
+  print >>f, "%f %f %f %f %f %f %f %f %f %f %f %f %f %f" \
+    %(xpos_cs[i],ypos_cs[i],zpos_cs[i], \
+    plx_cs[i],bt_cs[i],vt_cs[i],velz_cs[i], \
+      pmraerr_cs[i],pmdecerr_cs[i],hrverr_cs[i],hrv_cs[i],vlon_cs[i] \
+      ,vlat_cs[i],logg_cs[i])
   i+=1
 f.close()
 
@@ -263,9 +291,9 @@ print ' number of stars in bin in South=',nsz_s
 
 # vzm and vz2m
 vzmhist_n=np.histogram(zpos_cs,nbin_n,(zmin_n,zmax_n),weights=velz_cs)[0]/nsz_n
-vzmhist_s=np.histogram(zpos_cs,nbin_n,(zmin_s,zmax_s),weights=velz_cs)[0]/nsz_s
+vzmhist_s=np.histogram(zpos_cs,nbin_s,(zmin_s,zmax_s),weights=velz_cs)[0]/nsz_s
 vz2mhist_n=np.histogram(zpos_cs,nbin_n,(zmin_n,zmax_n),weights=(velz_cs**2))[0]/nsz_n
-vz2mhist_s=np.histogram(zpos_cs,nbin_n,(zmin_s,zmax_s),weights=(velz_cs**2))[0]/nsz_s
+vz2mhist_s=np.histogram(zpos_cs,nbin_s,(zmin_s,zmax_s),weights=(velz_cs**2))[0]/nsz_s
 # sig_vz
 sigvz_n=np.sqrt(vz2mhist_n-vzmhist_n**2)
 sigvz_s=np.sqrt(vz2mhist_s-vzmhist_s**2)
@@ -274,10 +302,11 @@ print ' vz dispersion North=',sigvz_n
 print ' vz dispersion South=',sigvz_s
 
 # show histogram
-plt.scatter(zbin_n,sigvz_n)
-plt.scatter(zbin_s,sigvz_s)
+plt.scatter(zpos_cs,velz_cs,marker='.')
+plt.scatter(zbin_n,sigvz_n,marker='o',color='red',s=50)
+plt.scatter(zbin_s,sigvz_s,marker='o',color='red',s=50)
 plt.xlabel(r"z (kpc)",fontsize=18,fontname="serif")
-plt.ylabel(r"Ns",fontsize=18,fontname="serif")
+plt.ylabel(r"Vz (km/s)",fontsize=18,fontname="serif")
 #plt.axis([-0.25,0.25,0.0,100])
 plt.xticks(fontsize=18)
 plt.yticks(fontsize=18)
