a6720b06119aa597842dbae7555214c338d03dae
Daisuke Kawata <d.kawata@ucl.ac.uk>
2017-02-17 17:26:21 +0800

    HOM2M vel2m with omega fitting.

--- py/hom2m.py
+++ py/hom2m.py
@@ -594,7 +594,7 @@ def run_m2m_weights_zsun_densv2m(w_init,A_init,phi_init,
        2016-12-06 - Written - Bovy (UofT/CCA)
        2016-12-09 - Added output_wevolution - Bovy (UofT/CCA)
        2016-12-16 - Added explicit noise in the observed densities - Bovy (UofT/CCA)
-       2017-02-07 - modified from run_m2m_weights_zsun
+       2017-02-07 - modified from run_m2m_weights_zsun - Kawata (MSSL/UCL)
     """
     w_out= copy.deepcopy(w_init)
     zsun_out= []
@@ -668,3 +668,180 @@ def run_m2m_weights_zsun_densv2m(w_init,A_init,phi_init,
         out= out+(wevol,rndindx,)
     return out
 
+# Functions that return the difference in z and vz for orbits starting at the same (z,vz)_init integrated
+# in potentials with different omega
+def zdiff(z_init,vz_init,omega1,omega2,t):
+    A1= numpy.sqrt(z_init**2.+vz_init**2./omega1**2.)
+    A2= numpy.sqrt(z_init**2.+vz_init**2./omega2**2.)
+    phi1= numpy.arctan2(-vz_init/omega1,z_init)
+    phi2= numpy.arctan2(-vz_init/omega2,z_init)
+    return A2*numpy.cos(omega2*t+phi2)-A1*numpy.cos(omega1*t+phi1)
+def vzdiff(z_init,vz_init,omega1,omega2,t):
+    A1= numpy.sqrt(z_init**2.+vz_init**2./omega1**2.)
+    A2= numpy.sqrt(z_init**2.+vz_init**2./omega2**2.)
+    phi1= numpy.arctan2(-vz_init/omega1,z_init)
+    phi2= numpy.arctan2(-vz_init/omega2,z_init)
+    return -A2*omega2*numpy.sin(omega2*t+phi2)+A1*omega1*numpy.sin(omega1*t+phi1)
+
+##### run force of change for omega from density and v^2
+
+def force_of_change_omega_densv2m(w_m2m,zsun_m2m,omega_m2m,
+                          z_m2m,vz_m2m,z_prev,vz_prev,
+                          step,eps_omega,eps,eps_vel,mu,w_prior,
+                          z_obs,dens_obs,dens_obs_noise,v2m_obs,v2m_obs_noise,
+                          h_m2m=0.02,delta_omega=0.3,
+                          delta_m2m=None,deltav2m_m2m=None):
+    # Compute the force of change by directly finite difference of the objective function
+    # REAL HACK FOLLOWS!
+    fcw, delta_m2m_do= force_of_change_weights(w_out,zsun_m2m,
+                                               z_m2m+zdiff(z_prev,vz_prev,omega_m2m,omega_m2m+delta_omega,step),
+                                               vz_m2m+vzdiff(z_prev,vz_prev,omega_m2m,omega_m2m+delta_omega,step),
+                                               eps,mu,w_init,
+                                               z_obs,dens_obs,dens_obs_noise,
+                                               h_m2m=h_m2m,
+                                               delta_m2m=None)
+    fcwv2, deltav2m_m2m_do= force_of_change_weights_v2m(w_out,zsun_m2m,
+                                                      z_m2m+zdiff(z_prev,vz_prev,omega_m2m,omega_m2m+delta_omega,step),
+                                                      vz_m2m+vzdiff(z_prev,vz_prev,omega_m2m,omega_m2m+delta_omega,step),
+                                                      eps_vel,mu,w_init,
+                                                      z_obs,v2m_obs,v2m_obs_noise,
+                                                      h_m2m=h_m2m,
+                                                      deltav2m_m2m=None)
+
+    return -2.*eps_omega\
+        *numpy.sum(delta_m2m*(delta_m2m_do-delta_m2m)+deltav2m_m2m*(deltav2m_m2m_do-deltav2m_m2m))/delta_omega
+
+##### run M2M fitting Omega with density and v^2 constraints.
+
+def run_m2m_weights_omega_densv2m(w_init,A_init,phi_init,
+                          omega_m2m,zsun_m2m,
+                          z_obs,dens_obs,dens_obs_noise,v2m_obs,v2m_obs_noise,
+                          step=0.001,nstep=1000,skipomega=10,
+                          eps=0.1,eps_vel=0.1,eps_omega=0.001,mu=1.,
+                          h_m2m=0.02,delta_omega=0.3,
+                          smooth=None,
+                          output_wevolution=False):
+    """
+    NAME:
+       run_m2m_weights_omega
+    PURPOSE:
+       Run M2M on the harmonic-oscillator data to optimize the weights of the orbits and the potential parameter
+       omega using both density and velocity data
+    INPUT:
+       w_init - initial weights [N]
+       A_init - initial zmax [N]
+       phi_init - initial angle (rad) [N]
+       omega_m2m - potential parameter omega
+       zsun_m2m - Sun's height above the plane [N]
+       z_obs - heights at which the density observations are made
+       dens_obs - observed densities
+       dens_obs_noise - noise in the observed densities
+       v2m_obs - observed velocity-squareds
+       v2m_obs_noise - noise in the observed velocity-squareds
+       step= stepsize of orbit integration
+       nstep= number of steps to integrate the orbits for
+       skipomega= only update omega every skipomega steps
+       eps= M2M epsilon parameter
+       eps_vel= M2M epsilon parameter for the velocity change
+       eps_omega= M2M epsilon parameter for omega force of change
+       mu= M2M entropy parameter mu
+       h_m2m= kernel size parameter for computing the observables
+       delta_omega= (0.3) difference in omega to use to compute derivative of objective function wrt omega
+       smooth= smoothing parameter alpha (None for no smoothing)
+       nodens= if True, don't fit the density data (also doesn't include entropy term)
+       output_wevolution= if set to an integer, return the time evolution of this many randomly selected weights
+    OUTPUT:
+       ((w_out,omega_out,z_out,vz_out),Q_out,[wevol,rndindx]) - 
+               ((output weights [N],output omega [nstep],final z [N], final vz [N]),
+                objective function as a function of time,
+                [weight evolution for randomly selected weights,index of random weights])
+    HISTORY:
+       2016-12-07 - Written - Bovy (UofT/CCA)
+       2016-12-09 - Added output_wevolution - Bovy (UofT/CCA)
+       2016-12-16 - Added explicit noise in the observed densities and densities x velocity-squared - Bovy (UofT/CCA)
+       2017-02-17 - Modified to use v2m_obs - Kawata (MSSL/UCL)
+    """
+    w_out= copy.deepcopy(w_init)
+    Q_out= []
+    omega_out= []
+    if output_wevolution:
+        rndindx= numpy.random.permutation(len(w_out))[:output_wevolution]
+        wevol= numpy.zeros((output_wevolution,nstep))
+    if not smooth is None:
+        # Smooth the constraints
+        phi_now= phi_init
+        z_m2m= A_init*numpy.cos(phi_now)
+        vz_m2m= -A_init*omega_m2m*numpy.sin(phi_now) # unnecessary
+        dens_init= compute_dens(z_m2m,zsun_m2m,z_obs,h_m2m,w=w_init)
+        delta_m2m= (dens_init-dens_obs)/dens_obs_noise
+        v2m_init= compute_v2m(z_m2m,vz_m2m,zsun_m2m,z_obs,h_m2m,w=w_init)
+        deltav2m_m2m= (v2m_init-v2m_obs)/v2m_obs_noise
+    else:
+        delta_m2m= None
+        deltav2m_m2m= None
+    z_m2m= A_init*numpy.cos(phi_init)
+    vz_m2m= -A_init*omega_m2m*numpy.sin(phi_init)
+    # Initialize
+    z_m2m_prev= A_init*numpy.cos(phi_init)
+    vz_m2m_prev= -A_init*omega_m2m*numpy.sin(phi_init)
+    ocounter= 0
+    for ii in range(nstep):
+        # Compute current (z,vz)
+        phi_prev= numpy.arctan2(-vz_m2m,z_m2m*omega_m2m)
+        A_now= numpy.sqrt(z_m2m**2.+vz_m2m**2./omega_m2m**2.)
+        phi_now= phi_prev+omega_m2m*step
+        z_m2m= A_now*numpy.cos(phi_now)
+        vz_m2m= -A_now*omega_m2m*numpy.sin(phi_now)
+        fcw, delta_m2m_new= force_of_change_weights(w_out,zsun_m2m,
+                                                    z_m2m,vz_m2m,
+                                                    eps,mu,w_init,
+                                                    z_obs,dens_obs,dens_obs_noise,
+                                                    h_m2m=h_m2m,
+                                                    delta_m2m=delta_m2m)
+        fcwv2, deltav2m_m2m_new= force_of_change_weights_v2m(w_out,zsun_m2m,
+                                                           z_m2m,vz_m2m,
+                                                           eps_vel,mu,w_init,
+                                                           z_obs,v2m_obs,v2m_obs_noise,
+                                                           h_m2m=h_m2m,
+                                                           deltav2m_m2m=deltav2m_m2m)
+        w_out+= step*fcw+step*fcwv2
+        w_out/= numpy.sum(w_out)/len(A_init)
+        w_out[w_out < 0.]= 10.**-16. 
+        Q_out.append(delta_m2m_new**2.+deltav2m_m2m_new**2.)
+        # Increment smoothing
+        if not smooth is None:
+            delta_m2m= step*smooth*(delta_m2m_new-delta_m2m)
+            deltav2m_m2m= step*smooth*(deltav2m_m2m_new-deltav2m_m2m)
+        if output_wevolution:
+            wevol[:,ii]= w_out[rndindx]
+        # Update omega in this step?
+        ocounter+= 1
+        if ocounter != skipomega:
+            omega_out.append(omega_m2m)
+            continue
+        ocounter= 0
+        if smooth is None:
+            fcomega= force_of_change_omega_densv2m(w_out,zsun_m2m,omega_m2m,
+                                           z_m2m,vz_m2m,z_m2m_prev,vz_m2m_prev,
+                                           step*skipomega,eps_omega,eps,eps_vel,mu,w_init,
+                                           z_obs,dens_obs,dens_obs_noise,v2m_obs,v2m_obs_noise,
+                                           h_m2m=h_m2m,delta_omega=delta_omega,
+                                           delta_m2m=delta_m2m_new,deltav2m_m2m=deltav2m_m2m_new)
+        else:
+            fcomega= force_of_change_omega_densv2m(w_out,zsun_m2m,omega_m2m,
+                                           z_m2m,vz_m2m,z_m2m_prev,vz_m2m_prev,
+                                           step*skipomega,eps_omega,eps,eps_vel,mu,w_init,
+                                           z_obs,dens_obs,dens_obs_noise,v2m_obs,v2m_obs_noise,
+                                           h_m2m=h_m2m,delta_omega=delta_omega,
+                                           delta_m2m=delta_m2m_new,deltav2m_m2m=deltav2m_m2m)
+        domega= step*skipomega*fcomega
+        maxdomega= delta_omega/10.
+        if numpy.fabs(domega) > maxdomega: domega= maxdomega*numpy.sign(domega)
+        omega_m2m+= domega
+        z_m2m_prev= copy.copy(z_m2m)
+        vz_m2m_prev= copy.copy(vz_m2m)
+        omega_out.append(omega_m2m)
+    out= ((w_out,numpy.array(omega_out),z_m2m,vz_m2m),numpy.array(Q_out))
+    if output_wevolution:
+        out= out+(wevol,rndindx,)
+    return out
